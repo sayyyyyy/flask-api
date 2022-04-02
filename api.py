a07c75c4cdc92a7ecafcf0d39eaec31665891274
sayyyyy <190781001@ccmailg.meijo-u.ac.jp>
2022-04-02 12:28:40 +0900

    条件指定追加

--- api.py
+++ api.py
@@ -4,9 +4,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def return_data():
-    data = {1: "あ",
-            2: "い",
-            3: "う",
-            4: "え",
-            5: "お"}
+    data = {1: "a",
+            2: "b",
+            3: "c",
+            4: "d",
+            5: "e"}
+    return data
+
+
+@app.route('/id=<id>')
+def designated_return_data(id):
+
+    if id == '1':
+        data = {1: "a",
+                2: "b",
+                3: "c",
+                4: "d",
+                5: "e"}
+    elif id == '2':
+        data = {1: "あ",
+                2: "い",
+                3: "う",
+                4: "え",
+                5: "お"}
+    else:
+        data = {1: "Not Found"}   
+    
     return data
